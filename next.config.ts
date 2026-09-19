a50fac9b59e2e3bbc4dc282dd19b83c428c470c3
João Paulo Gonçalves Santos <github.broadcast509@passinbox.com>
2026-09-19 11:18:02 -0300

    feat: add Sentry monitoring

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import { withSerwist } from "@serwist/turbopack";
+import { withSentryConfig } from "@sentry/nextjs/config";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
 };
 
-export default withSerwist(nextConfig);
+export default withSentryConfig(withSerwist(nextConfig), {
+  org: process.env.SENTRY_ORG ?? "joao-paulo-goncalves-santos",
+  project: process.env.SENTRY_PROJECT ?? "pethub",
+  silent: true,
+  webpack: {
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
